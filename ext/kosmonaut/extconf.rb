f683cc1238231e006e40f2af927c7ff1c01eb4ce
Chris Kowalik <chris@nu7hat.ch>
2012-01-16 01:16:26 -0200

    fixes and cleanups

--- ext/kosmonaut/extconf.rb
+++ ext/kosmonaut/extconf.rb
@@ -3,8 +3,9 @@ require 'mkmf'
 
 have_library('pthread')
 have_library('uuid', 'uuid_generate')
-have_header('uuid/uuid.h')
 have_header('string.h')
+
+$CFLAGS = "-O2 -pipe -fPIC -mtune=generic"
 
 ext_dir = File.expand_path("..", __FILE__)
 $CFLAGS << " -I#{ext_dir}"
