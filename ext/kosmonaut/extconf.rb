c955f9c10f6c4131360fa3a6d773c152dca107f9
Chris Kowalik <chris@nu7hat.ch>
2012-01-16 00:57:25 -0200

    uuid replaced with urandom

--- ext/kosmonaut/extconf.rb
+++ ext/kosmonaut/extconf.rb
@@ -6,6 +6,8 @@ have_library('uuid', 'uuid_generate')
 have_header('uuid/uuid.h')
 have_header('string.h')
 
+ext_dir = File.expand_path("..", __FILE__)
+$CFLAGS << " -I#{ext_dir}"
 include_dir = File.expand_path("../../include", __FILE__)
 $CFLAGS << " -I#{include_dir}"
 
--- ext/kosmonaut/libkosmonaut/src/utils.c
+++ ext/kosmonaut/libkosmonaut/src/utils.c
@@ -1,26 +1,33 @@
-#include <uuid/uuid.h>
+/*#include <uuid/uuid.h>*/
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <assert.h>
 
-#define UUIDSTR_SIZE 36
+#define SESSID_SIZE 35
 
 char*
 generate_identity (const char* type,
                    const char* vhost,
                    const char* secret)
 {
-    int identity_len = strlen(type) + strlen(vhost) + strlen(secret) + UUIDSTR_SIZE + 3;
+    int i = 0;
+    int identity_len = strlen(type) + strlen(vhost) + strlen(secret) + SESSID_SIZE + 3;
     char* identity = (char*)malloc(identity_len * sizeof(char));
-    char* sessid = (char*)malloc(UUIDSTR_SIZE * sizeof(char));
-    uuid_t uuid;
+    char* sessid = (char*)malloc(SESSID_SIZE * sizeof(char));
+    FILE* randdata = fopen("/dev/urandom", "rb");
 
-    uuid_generate(uuid);
-    uuid_unparse_lower(uuid, sessid);
+    assert(randdata);
+    while (i < SESSID_SIZE) {
+        unsigned short c = fgetc(randdata);
+        sprintf(sessid + i, "%x", c);
+        i += 1;
+    }
+    
     sprintf(identity, "%s:%s:%s:%s", type, vhost, secret, sessid);
-
-    uuid_clear(uuid);
+    fclose(randdata);
     free(sessid);
 
     return identity;
