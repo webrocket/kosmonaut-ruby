b0c85368b20730144b8a1405bdf4fdcb2bfda8e5
Chris Kowalik <dev+nu7hatch@cuboxsa.com>
2012-01-16 22:25:13 -0200

    fixed worker listen

--- lib/kosmonaut/worker.rb
+++ lib/kosmonaut/worker.rb
@@ -7,7 +7,6 @@ module Kosmonaut
 
     RECONNECT_DELAY = 1000 # in milliseconds
     HEARTBEAT_INTERVAL = 2000 # in milliseconds
-    LIVENESS = 3
 
     def initialize(url)
       super(url)
@@ -17,26 +16,27 @@ module Kosmonaut
       @reconnect_delay = RECONNECT_DELAY
       @heartbeat_ivl = HEARTBEAT_INTERVAL
       @heartbeat_at = 0
-      @liveness = 0
     end
 
     def listen
-      reconnect
       @alive = true
+      reconnect
       while true
-        if !alive?
-          send(@sock, ["QT"])
-          disconnect
-          break
-        end
         begin
+          if !alive?
+            send(@sock, ["QT"]) if @sock
+            disconnect
+            break
+          end
+          unless @sock
+            raise Errno::ECONNREFUSED
+          end
           Timeout.timeout(((@heartbeat_ivl * 2).to_f / 1000.0).to_i + 1) {
             msg = recv(@sock)
-            @liveness = LIVENESS
+            raise Errno::ECONNRESET if @sock.eof? || msg.empty?
             log("Worker/RECV : #{msg.join("\n").inspect}")
-            next if msg.size < 1
             cmd = msg.shift
-          
+              
             case cmd
             when "HB"
               # nothing to do...
@@ -49,13 +49,12 @@ module Kosmonaut
               error_handler(msg.size < 1 ? 597 : msg[0])
             end
           }
-        rescue Timeout::Error, Errno::ECONNRESET
-          if (@liveness -= 1) == 0
-            sleep(@reconnect_delay.to_f / 1000.0)
-            reconnect
-          end
+        rescue Timeout::Error, Errno::ECONNRESET, Errno::ECONNREFUSED => err
+          log("Worker/RECONNECT: " + err.to_s)
+          sleep(@reconnect_delay.to_f / 1000.0)
+          reconnect
         end
-        if Time.now.to_f > @heartbeat_at
+        if Time.now.to_f > @heartbeat_at && @sock
           send(@sock, ["HB"])
           @heartbeat_at = Time.now.to_f + (@heartbeat_ivl.to_f / 1000.0)
         end
@@ -92,9 +91,9 @@ module Kosmonaut
     def reconnect
       disconnect
       @sock = connect
-      @sock.write(pack(["RD"]))
-      @liveness = LIVENESS
-      @heartbeat_at = Time.now.to_f + (@heartbeat_ivl.to_f / 1000.0)
+      send(@sock, ["RD"])
+      @heartbeat_at = Time.now.to_f + (@heartbeat_ivl.to_f / 1000.0)      
+    rescue Errno::ECONNREFUSED
     end
 
     def message_handler(data)
